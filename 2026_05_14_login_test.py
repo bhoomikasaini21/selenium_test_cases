7093bb8db81967f76b3ecb7f1fdcc50b831e6c31
bhoomikasaini21 <106151651+bhoomikasaini21@users.noreply.github.com>
2026-05-14 16:54:53 +0530

    Update 2026_05_14_login_test.py

--- 2026_05_14_login_test.py
+++ 2026_05_14_login_test.py
@@ -2,31 +2,26 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 import time
 
-# Open browser
-driver = webdriver.Chrome()
 
-# Open website
+driver = webdriver.Chrome()
 driver.get("https://www.saucedemo.com/")
 
-# Maximize window
 driver.maximize_window()
 
-# Enter username
+
 username = driver.find_element(By.ID, "user-name")
 username.send_keys("standard_user")
 
-# Enter password
+
 password = driver.find_element(By.ID, "password")
 password.send_keys("secret_sauce")
 
-# Click login button
+
 login_button = driver.find_element(By.ID, "login-button")
 login_button.click()
 
-# Wait for page load
-time.sleep(3)
 
-# Verify login success
+time.sleep(3)
 current_url = driver.current_url
 
 if "inventory" in current_url:
@@ -34,5 +29,4 @@ if "inventory" in current_url:
 else:
     print("TEST CASE FAILED")
 
-# Close browser
 driver.quit()
